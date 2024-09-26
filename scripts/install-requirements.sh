ffa6f5ec2b5ffd9c5dde88af366cf6fae9f27250
Henil Malaviya <henilm.dev@gmail.com>
2024-09-27 03:04:58 +0530

    chore: add oh-my-zsh install script

--- scripts/install-requirements.sh
+++ scripts/install-requirements.sh
@@ -7,6 +7,9 @@ sudo apt-get install -y \
 	zsh \
 	fzf
 
+echo "Installing Oh-My-Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 echo "Installing zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
